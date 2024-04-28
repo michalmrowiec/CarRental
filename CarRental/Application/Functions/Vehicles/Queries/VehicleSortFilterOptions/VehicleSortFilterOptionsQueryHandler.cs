50e56b5b0c6fbf9f2938b539ea656f24f716be6b
Michał Mrowiec <michalmro210101@gmail.com>
2024-04-28 12:58:11 +0200

    Added brand select filter in UI vehicle list

---
+++ CarRental/Application/Functions/Vehicles/Queries/VehicleSortFilterOptions/VehicleSortFilterOptionsQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using CarRental.Application.Contracts;
+using MediatR;
+
+namespace CarRental.Application.Functions.Vehicles.Queries.VehicleSortFilterOptions
+{
+    public class VehicleSortFilterOptionsQueryHandler : IRequestHandler<VehicleSortFilterOptionsQuery, object>
+    {
+        private readonly IVehicleRepository _vehicleRepository;
+
+        public VehicleSortFilterOptionsQueryHandler(IVehicleRepository vehicleRepository)
+        {
+            _vehicleRepository = vehicleRepository;
+        }
+        public async Task<object> Handle(VehicleSortFilterOptionsQuery request, CancellationToken cancellationToken)
+        {
+            var vehicles = await _vehicleRepository.GetAllAsync();
+            var options =
+                new
+                {
+                    Brand = vehicles.Select(v => v.Brand).Distinct().Order().ToList(),
+                    BodyType = vehicles.Select(v => v.BodyType).Distinct().ToList(),
+                    Seats = vehicles.Select(v => v.Seats).Distinct().ToList(),
+                    GearboxType = vehicles.Select(v => v.GearboxType).Distinct().ToList(),
+                    EnginePower = vehicles.Select(v => v.EnginePower).Distinct().ToList(),
+                    YearOfProduction = vehicles.Select(v => v.YearOfProduction).Distinct().ToList()
+                };
+
+            return options;
+        }
+    }
+}
--- CarRental/ClientApp/src/components/VehiclesPage/VehiclesList.js
+++ CarRental/ClientApp/src/components/VehiclesPage/VehiclesList.js
@@ -13,22 +13,52 @@ export class VehiclesList extends Component {
             currentPage: 0,
             totalItems: 0,
             from: null,
-            to: null
+            to: null,
+            brandsToFiltier: { 'Wszystkie': '' },
+            brand: 'Wszystkie'
         };
     }
 
     componentDidMount() {
+        this.getOptions();
         this.giveVehicles(this.state.page, this.state.pageSize, null, null);
     }
 
-    async giveVehicles(page, pageSize, fromDate, toDate) {
+    async getOptions() {
+        const response = await fetch('https://localhost:44403/api/v1/Vehicle/get-filtered', {
+            method: 'OPTIONS',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (response.status === 200) {
+            const data = await response.json();
+
+            let brands = {
+                'Wszystkie': '', ...data.brand.reduce((obj, item) => {
+                    obj[item] = item;
+                    return obj;
+                }, {})
+            };
+
+            this.setState({
+                brandsToFiltier: brands
+            });
+        }
+    }
+
+    async giveVehicles(page, pageSize) {
         if (pageSize === undefined)
             pageSize = this.state.pageSize;
 
-        if (fromDate > toDate) {
-            fromDate = null;
-            toDate = null;
+        if (this.state.from > this.state.to) {
+            this.setState({
+                from: '',
+                to: ''
+            })
         }
+
 
         const response = await fetch('https://localhost:44403/api/v1/Vehicle/get-filtered', {
             method: 'POST',
@@ -36,20 +66,18 @@ export class VehiclesList extends Component {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                filters: '',
+                filters: 'brand@=' + this.state.brandsToFiltier[this.state.brand],
                 sorts: '',
                 page: page,
                 pageSize: pageSize,
-                from: fromDate,
-                to: toDate
+                from: this.state.from === '' ? null : this.state.from,
+                to: this.state.to === '' ? null : this.state.to
             })
         });
 
         if (response.status === 200) {
-            console.log("downloaded");
 
             const data = await response.json();
-            console.log(data);
 
             const vehicles = data.items.map(item => ({
                 name: item.brand + ' ' + item.model,
@@ -68,21 +96,37 @@ export class VehiclesList extends Component {
         }
     }
 
+    handleBrandChange = (event) => {
+        this.setState({ brand: event.target.value });
+    }
+
     render() {
-        const { vehicles, pageSize, currentPage, totalPages } = this.state;
+        const { vehicles, pageSize, currentPage, totalPages, brandsToFiltier, brand } = this.state;
+        console.log(brandsToFiltier);
         return (
             <div className="container">
 
+                <select value={brand} onChange={this.handleBrandChange}>
+                    {Object.keys(brandsToFiltier).map((key) => (
+                        <option key={key} value={key}>
+                            {key}
+                        </option>
+                    ))}
+                </select>
+
                 <div>
                     <label>From: </label>
-                    <input type="date" onChange={(e) => this.setState({ from: e.target.value })} />
+                    <input type="date" onChange={(e) => this.setState({ from: e.target.value })} value={this.state.from} />
                     <label>To: </label>
-                    <input type="date" onChange={(e) => this.setState({ to: e.target.value })} />
-                    <button onClick={() => this.giveVehicles(1, this.state.pageSize, this.state.from, this.state.to)}>Search</button>
-
+                    <input type="date" onChange={(e) => this.setState({ to: e.target.value })} value={this.state.to} />
                     <button onClick={() => {
                         this.giveVehicles(1, this.state.pageSize)
-                    }}>All</button>
+                    }}>Filtruj</button>
+
+                    <button onClick={() => {
+                        this.setState({ from: '', to: '', brand: 'Wszystkie' });
+                        this.giveVehicles(1, this.state.pageSize);
+                    }}>Wyczyść filtry</button>
                 </div>
 
                 {vehicles.map((vehicle, index) => (
