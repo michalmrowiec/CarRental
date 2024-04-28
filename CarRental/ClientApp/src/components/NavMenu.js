77ab96d55fc0cb463d2ddb8bea53f5fad7a19e5f
Marcin Kuśnierz <marcinkusnierz91@gmail.com>
2024-04-28 10:37:28 +0200

    Logout action improved

--- CarRental/ClientApp/src/components/NavMenu.js
+++ CarRental/ClientApp/src/components/NavMenu.js
@@ -1,120 +1,100 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Collapse, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
 import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
-
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import '../style/NavMenu.css';
 import carLogo from '../images/car_rental.svg';
 import userLogo from '../images/user.svg';
 
-// import { AuthContext } from './AuthContext';
+const NavMenu = () => {
+    const [collapsed, setCollapsed] = useState(true);
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(sessionStorage.getItem('userRole') !== null);
+    const [role, setRole] = useState(sessionStorage.getItem('userRole'));
+    const navigate = useNavigate();
+    const dropdownRef = useRef(null);
 
+    const toggleNavbar = () => setCollapsed(!collapsed);
+    const toggleDropdown = () => setDropdownOpen(prevState => !prevState);
 
-export class NavMenu extends Component {
-    constructor(props) {
-        super(props);
-
-        this.toggleNavbar = this.toggleNavbar.bind(this); // Dodaj tę linię
-        this.toggleDropdown = this.toggleDropdown.bind(this);
-        this.handleLogout = this.handleLogout.bind(this);
-        this.state = {
-            dropdownOpen: false,
-            isLoggedIn: sessionStorage.getItem('userRole') !== null,
-            collapsed: true,
-            role: sessionStorage.getItem('userRole')
-        };
-    }
-
-
-    toggleNavbar() {
-        this.setState({
-            collapsed: !this.state.collapsed
-        });
-    }
-
-    componentDidUpdate(prevProps, prevState) {
-        const isLoggedIn = sessionStorage.getItem('userRole') !== null;
-        console.log(prevState.isLoggedIn);
-        if (prevState.isLoggedIn !== isLoggedIn) {
-            this.setState({ isLoggedIn });
-            // window.location.reload();
-        }
-    }
-
-
-
-    toggleDropdown() {
-        this.setState(prevState => ({
-            dropdownOpen: !prevState.dropdownOpen
-        }));
-    }
-
-    handleLogout() {
-        // Usuń informacje o zalogowanym użytkowniku z localStorage
+    const handleLogout = () => {
         sessionStorage.removeItem('userRole');
-        // window.location.reload();
+        navigate('/'); // Przekierowanie do strony głównej
+        window.location.reload();
+    };
 
-    }
+    useEffect(() => {
+        const isLoggedIn = sessionStorage.getItem('userRole') !== null;
+        setIsLoggedIn(isLoggedIn);
+        setRole(sessionStorage.getItem('userRole'));
+    }, []);
 
-    render() {
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+                setDropdownOpen(false);
+            }
+        };
 
-        // const role = sessionStorage.getItem('userRole'); // Pobierz rolę z localStorage
-        // console.log(role);
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
 
-        // console.log(this.state.role);
-        // console.log(this.state.isLoggedIn);
-        return (
-            <header>
-                <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container light>
-                    <NavbarBrand tag={Link} to="/">
-                        <img src={carLogo} alt='CarLogo' className='mr-2'></img>
-                        CarRental
-                    </NavbarBrand>
-                    <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-                    <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
-                        <ul className="navbar-nav flex-grow">
-                            <NavItem>
-                                <NavLink tag={Link} className="text-dark text-decoration" to="/">Home</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink tag={Link} className="text-dark text-decoration" to="/AboutUs">AboutUs</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink tag={Link} className="text-dark text-decoration" to="/vehicles">Vehicles</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink tag={Link} className="text-dark text-decoration" to="/vehiclesList">Vehicles List</NavLink>
-                            </NavItem>
+    return (
+        <header>
+            <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container light>
+                <NavbarBrand tag={Link} to="/">
+                    <img src={carLogo} alt='CarLogo' className='mr-2'></img>
+                    CarRental
+                </NavbarBrand>
+                <NavbarToggler onClick={toggleNavbar} className="mr-2" />
+                <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!collapsed} navbar>
+                    <ul className="navbar-nav flex-grow">
+                        <NavItem>
+                            <NavLink tag={Link} className="text-dark text-decoration" to="/">Home</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink tag={Link} className="text-dark text-decoration" to="/AboutUs">AboutUs</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink tag={Link} className="text-dark text-decoration" to="/vehicles">Vehicles</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink tag={Link} className="text-dark text-decoration" to="/vehiclesList">Vehicles List</NavLink>
+                        </NavItem>
 
-                            {this.state.role === 'customer' && this.state.isLoggedIn ? (
-                                <>
-                                    <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggleDropdown}>
-                                        <DropdownToggle caret className="btn btn-light d-flex align-items-center">
-                                            <div className="">
-                                                <img src={userLogo} alt='userLogo' className='mr-2 img-fluid user-logo'></img>
-                                                <span className="text-dark">{localStorage.getItem('loggedInUser')}</span>
-                                            </div>
-                                        </DropdownToggle>
-                                        <DropdownMenu>
-                                            <DropdownItem tag={Link} className="text-dark text-decoration" to="/User">My profile</DropdownItem>
-                                            <DropdownItem onClick={this.handleLogout} className="text-dark text-decoration">Log-Out</DropdownItem>
-                                        </DropdownMenu>
-                                    </Dropdown>
-                                </>
-                            ) : (
-                                <>
-                                    <NavItem>
-                                        <NavLink tag={Link} className="text-dark text-decoration" to="/Join">Join</NavLink>
-                                    </NavItem>
-                                    <NavItem>
-                                        <NavLink tag={Link} className="text-dark text-decoration" to="/SignIn">Sign-In</NavLink>
-                                    </NavItem>
-                                </>
-                            )}
-                        </ul>
-                    </Collapse>
-                </Navbar>
-            </header>
-        );
-    }
-}
+                        {role === 'customer' && isLoggedIn ? (
+                            <>
+                                <Dropdown isOpen={dropdownOpen} toggle={toggleDropdown} innerRef={dropdownRef}>
+                                    <DropdownToggle caret className="btn btn-light d-flex align-items-center">
+                                        <div className="">
+                                            <img src={userLogo} alt='userLogo' className='mr-2 img-fluid user-logo'></img>
+                                            <span className="text-dark">{localStorage.getItem('loggedInUser')}</span>
+                                        </div>
+                                    </DropdownToggle>
+                                    <DropdownMenu>
+                                        <DropdownItem tag={Link} className="text-dark text-decoration" to="/User">My profile</DropdownItem>
+                                        <DropdownItem onClick={handleLogout} className="text-dark text-decoration">Log-Out</DropdownItem>
+                                    </DropdownMenu>
+                                </Dropdown>
+                            </>
+                        ) : (
+                            <>
+                                <NavItem>
+                                    <NavLink tag={Link} className="text-dark text-decoration" to="/Join">Join</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink tag={Link} className="text-dark text-decoration" to="/SignIn">Sign-In</NavLink>
+                                </NavItem>
+                            </>
+                        )}
+                    </ul>
+                </Collapse>
+            </Navbar>
+        </header>
+    );
+};
+
+export default NavMenu;
